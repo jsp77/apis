8233b09f9b6ac8765bae033bf7e3443ef61315a4
jsp77 <jp072882@gmail.com>
2015-03-23 15:50:09 -0700

    comment

--- flickr/index.php
+++ flickr/index.php
@@ -16,30 +16,29 @@
 		var key = '403d5274ed5eb1c0131d74485aaa30dd';
 		var base_url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + key + "&format=json&nojsoncallback=1";
 
-		$(document).ready(function () {
 
-			var displayPhotos = function (photos) {
-				if(photos.length > 0){
-					$('#photo-list').html('');
-				}else{
-					$('#photo-list').html('No Photos Found');
+		$(document).ready(function () {var displayPhotos = function (photos) {  // function to grab photos from url into an array and attach them to created div and appends them.
+				if(photos.length > 0){               // if photo array is greater than 0
+					$('#photo-list').html('');       // clears the html of photo-list div 
+				}else{                               // else
+					$('#photo-list').html('No Photos Found'); // give an error in the div no photo found
 				}
-				for (var index in photos) {
-					var photoObj = photos[index];
+				for (var index in photos) {          // loops through the photo array
+					var photoObj = photos[index];    // set variable for the position of the array
 					var url = 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' + photoObj.secret + '.jpg'
-					var img = $('<img/>').attr('src', url).width(250);
-					$('#photo-list').append(img);
+					                                 // variable for the url to grab each photo
+                    var img = $('<img/>').attr('src', url).width(250);     // create an img tag with src and url and width of 250 of image
+					$('#photo-list').append(img);                          // append the created img to photo-div
 				}
 			}
 
-			$('body').on('click', '#search-btn', function () {
-				var val = $('#search').val();
-				var search_url = base_url + "&text=" + val;
-				var photos = [];
+			$('body').on('click', '#search-btn', function () { // function to get the photos from the url with the key. on click 
+				var val = $('#search').val();                  // create a variable to get the value of search input
+				var search_url = base_url + "&text=" + val;    // create a variable to add the url + the &text and the value from input
+				var photos = [];                               // created photo array thats being called in the displayPhotos function
 
-				$('#photo-list').html('Searching');
-
-				$.ajax({
+				$('#photo-list').html('Searching');            // display text to html
+				$.ajax({                                        
 					url: search_url,
 					dateType: 'json',
 					crossDomain: true,
